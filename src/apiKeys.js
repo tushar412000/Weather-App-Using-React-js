cbf3a154d6a752acfb72f0de64ebf5432791bb61
tushar412000 <110544909+tushar412000@users.noreply.github.com>
2023-03-23 18:52:31 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "8117765ea94ae9db4fdfa28f98ecb867",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
